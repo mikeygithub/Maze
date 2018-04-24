613b5b5559ee39b3c7e14a16ce164fcf6fa380b7
mikey <biaogejiushibiao@outlook.com>
2018-04-24 20:44:40 +0800

    20180424

---
+++ Maze/function.cpp
@@ -0,0 +1,6 @@
+/*#include"stdafx.h"
+#include"function.h"*/
+#include "stdio.h"
+int myMax(int a, int b) {
+	printf("hello");
+}
---
+++ Maze/function.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef _FUNCTIONH
+#define _FUNCTIONH
+
+
+int myMax(int a, int b);
+
+
+
+#endif
